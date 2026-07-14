46b095d3aaaaf4e70b050b79740be2a5cfcfff53
nuzul <amuhnuzul@gmail.com>
2026-07-14 22:06:12 +0800

    L3-3.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,25 +1,31 @@
 <nav>
-    <a href="{{ route('beranda') }}">
-        TokoKu
-    </a>
-
-    <a href="{{ route('beranda') }}">
+    <a
+        href="{{ route('beranda') }}"
+        class="{{ request()->routeIs('beranda') ? 'aktif' : '' }}"
+    >
         Beranda
     </a>
 
-    <a href="{{ route('produk.index') }}">
-        Semua Produk
+    <a
+        href="{{ route('produk.index') }}"
+        class="{{ request()->routeIs('produk.*') ? 'aktif' : '' }}"
+    >
+        Produk
     </a>
 
     @foreach ($daftarKategori as $slug => $namaKategori)
-
-        <a href="{{ route('kategori.show', $slug) }}">
+        <a
+            href="{{ route('kategori.show', $slug) }}"
+            class="{{ request()->routeIs('kategori.show') && request()->route('slug') === $slug ? 'aktif' : '' }}"
+        >
             {{ $namaKategori }}
         </a>
-
     @endforeach
 
-    <a href="{{ route('tentang') }}">
+    <a
+        href="{{ route('tentang') }}"
+        class="{{ request()->routeIs('tentang') ? 'aktif' : '' }}"
+    >
         Tentang
     </a>
 </nav>
